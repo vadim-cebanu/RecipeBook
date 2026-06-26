ba4eb24e5648f552ee3e49309ddc9f5ccf640f7c
vadim-cebanu <vadykdeejay@gmail.com>
2026-06-26 10:47:05 +0200

    Implement Recipe API with ViewSet and serializer

--- recipes_app/views.py
+++ recipes_app/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Recipe
+from .serializer import RecipeSerializer
 
-# Create your views here.
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
